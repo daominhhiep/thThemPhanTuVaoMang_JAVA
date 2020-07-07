1482aff35f8a695ff7f1269abf32e28364a3d8cd
admin <daominhhiep97@gmail.com>
2020-07-07 16:53:44 +0700

    [Bài tập] Thêm phần tử vào mảng

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,43 +3,43 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-            System.out.println("Enter size arr:");
-            Scanner scanner = new Scanner(System.in);
-            int sizearr = scanner.nextInt();
-            int[] arr = new int[sizearr];
-            System.out.println("Array is:");
-            for (int i = 0; i < arr.length; i++) {
-                    arr[i] = (int) (Math.random() * 9);
-                    System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            System.out.println("Enter the number: ");
-            int number = scanner.nextInt();
-            System.out.println("Enter the position in the array: ");
-            int index = scanner.nextInt();
-            addNumber(arr, index, number);
-    }
+        public static void main(String[] args) {
+                System.out.println("Enter size arr:");
+                Scanner scanner = new Scanner(System.in);
+                int sizearr = scanner.nextInt();
+                int[] arr = new int[sizearr];
+                System.out.println("Array is:");
+                for (int i = 0; i < arr.length; i++) {
+                        arr[i] = (int) (Math.random() * 9);
+                        System.out.print(arr[i] + " ");
+                }
+                System.out.println();
+                System.out.println("Enter the number: ");
+                int number = scanner.nextInt();
+                System.out.println("Enter the position in the array: ");
+                int index = scanner.nextInt();
+                addNumber(arr, index, number);
+        }
 
-    public static void addNumber(int[] arr, int index, int number) {
-            if (index >= 0 && index <= arr.length) {
-                    int[] arrnew = new int[arr.length + 1];
-                    for (int i = 0; i < arr.length; i++) {
-                            arrnew[i] = arr[i];
-                            if (i == index) {
-                                    arrnew[i] = number;
-                                    break;
-                            }
-                    }
-                    for (int i = index; i < arr.length; i++) {
-                            arrnew[i + 1] = arr[i];
-                    }
-                    System.out.print("New array:");
-                    for (int i = 0; i < arrnew.length; i++) {
-                            System.out.print(arrnew[i] + " ");
-                    }
-            } else {
-                    System.out.println("Cannot add element into array!");
-            }
-    }
+        public static void addNumber(int[] arr, int index, int number) {
+                if (index >= 0 && index <= arr.length) {
+                        int[] arrnew = new int[arr.length + 1];
+                        for (int i = 0; i < arr.length; i++) {
+                                arrnew[i] = arr[i];
+                                if (i == index) {
+                                        arrnew[i] = number;
+                                        break;
+                                }
+                        }
+                        for (int i = index; i < arr.length; i++) {
+                                arrnew[i + 1] = arr[i];
+                        }
+                        System.out.print("New array:");
+                        for (int i = 0; i < arrnew.length; i++) {
+                                System.out.print(arrnew[i] + " ");
+                        }
+                } else {
+                        System.out.println("Cannot add element into array!");
+                }
+        }
 }
